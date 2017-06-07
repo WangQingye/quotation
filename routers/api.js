ccbb3d6b77cdd8dbb1c9561ed6b1fc80da48ff66
wangqingye <329103586@qq.com>
2017-06-07 23:00:29 +0800

    注册成功存入数据库

--- routers/api.js
+++ routers/api.js
@@ -5,15 +5,17 @@
 
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 //统一返回格式
 var responseData;
 
-router.use(function () {
+router.use(function (req, res, next) {
    responseData = {
        code: 0,
        message: ''
-   }
+   };
+   next();
 });
 
 /*
@@ -29,6 +31,7 @@ router.post('/user/signup', function (req, res, next) {
     var password = req.body.password;
     var repassword = req.body.repassword;
 
+    //数据验证
     if(username === '')
     {
         responseData.code = 1;
@@ -38,17 +41,42 @@ router.post('/user/signup', function (req, res, next) {
     }
     if(password === '')
     {
-        responseData.code = 1;
+        responseData.code = 2;
         responseData.message = '密码不能为空';
         res.json(responseData);
         return;
     }
     if(password !== repassword)
     {
-        responseData.code = 1;
+        responseData.code = 3;
         responseData.message = '两次密码不一致';
         res.json(responseData);
+        return;
     }
+
+    /*数据库查询*/
+    User.findOne({
+       username:username
+    }).then(function (userInfo) {
+        console.log(userInfo);
+        if(userInfo)
+        {
+            responseData.code = 4;
+            responseData.message = '用户名已经被注册了';
+            res.json(responseData);
+        }else
+        {
+            var user = new User({
+                    username:username,
+                    password:password
+                });
+            return user.save();
+        }
+    }).then(function (newUserInfo) {
+        console.log(newUserInfo);
+        responseData.message = '注册成功';
+        res.json(responseData);
+    });
 });
 
 module.exports = router;
